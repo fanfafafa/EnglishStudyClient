6bb7dbd2ded047da0dbae70efbab86d22bf15cd2
樊希毅 <fanxiyi@hollysys.net>
2021-08-02 17:32:50 +0800

    Vue项目框架搭建。

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+	<div id="app">
+		<TopContent/>
+	</div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import TopContent from './components/TopContent.vue'
+
+export default {
+  name: 'app',
+  components: {
+	TopContent,
+    HelloWorld
+	
+  }
+}
+</script>
+
+<style>
+#app {
+}
+</style>
---
+++ src/components/TopContent.vue
@@ -0,0 +1,37 @@
+<template>
+	<div class="top">
+		<span class="title">English Study</span>
+		<Button class="button" :size="buttonSize" type="text" shape="circle">ADD</Button>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'TopContent',
+		data () {
+			return {
+				buttonSize: 'large'
+			}
+		}
+	}
+</script>
+
+<style scoped>
+	.top {
+		height: 50px;
+		background-color: #2d8cf0;
+		
+		color: #ffffff;
+		font-family: 'Avenir', Helvetica, Arial, sans-serif;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+	}
+	
+	.title {
+		line-height: 50px;
+		font-size: 26px;
+		font-weight: bold;
+		margin-left: 10px;
+	}
+	
+</style>
